cd5101e953f32cfab7b2d70353113ea1d62d01e6
Rodrigo Alejandro Meneses <meneses.rodrigo@hotmail.com>
2021-12-14 22:38:42 -0500

    Integracion listar usuarios
    Integracion listar usuarios

--- frontend/src/components/Gestionusuarios.js
+++ frontend/src/components/Gestionusuarios.js
@@ -1,79 +1,100 @@
 import React from 'react'
 import '../App.css'
+import GET_USUARIOS from '../Apollo/gql/getUsuarios'
+import { useQuery } from '@apollo/client';
 
 
 const Gestionusuarios = () => {
+
+    const { loading, data, error } = useQuery(GET_USUARIOS);
+
+
+
     return (
-        <div className='login2'>
+
+        <>
+            {loading && <p>Cargando ...</p>}
+            {error && <p>Se ha producido un error</p>}
+            {
+                data &&
 
 
-            <div class="encabezado">
-                <h5>Usuarios</h5>
-            </div>
-
-            <div class="iconosVentas">
+                <div className='login2'>
 
 
-            </div>
-            <div class="filtro">
-                <span>Buscar</span>
-                <input type="text" class="redondo" />
-            </div>
-            <div class="divTabla">
-                <table>
-                    <tr>
-                        <th>Id Usuario</th>
-                        <th>Nombre</th>
-                        <th>Rol</th>
-                        <th>Correo</th>
-                        <th>Estado</th>
-                    </tr>
-                    <tr>
-                        <td>Dato</td>
-                        <td>Dato</td>
-                        <td>Dato</td>
-                        <td>Dato</td>
-                        <td><select >
-                            <option>Pendiente</option>
-                            <option>Autorizado</option>
-                            <option>No Autorizado</option>
-                        </select></td>
-                    </tr>
-                    <tr>
-                        <td>Dato</td>
-                        <td>Dato</td>
-                        <td>Dato</td>
-                        <td>Dato</td>
-                        <td><select >
-                            <option>Pendiente</option>
-                            <option>Autorizado</option>
-                            <option>No Autorizado</option>
-                        </select></td>
+                    <div class="encabezado">
+                        <h5>Usuarios</h5>
+                    </div>
 
-                    </tr>
-                    <tr>
-                        <td>Dato</td>
-                        <td>Dato</td>
-                        <td>Dato</td>
-                        <td>Dato</td>
-                        <td><select >
-                            <option>Pendiente</option>
-                            <option>Autorizado</option>
-                            <option>No Autorizado</option>
-                        </select></td>
-                    </tr>
-                </table>
+                    <div class="iconosVentas">
 
-                <div class="acciones" align="center">
-                    <h5>
-                        <button class="Guardar">
-                            Guardar
-                        </button>
-                    </h5>
+
+                    </div>
+                    <div class="filtro">
+                        <span>Buscar</span>
+                        <input type="text" class="redondo" />
+                    </div>
+                    <div class="divTabla">
+                        <table>
+
+                            <thead>
+
+                                <tr>
+                                    <th>Id Usuario</th>
+                                    <th>Nombre</th>
+                                    <th>Rol</th>
+                                    <th>Correo</th>
+                                    <th>Estado</th>
+                                </tr>
+
+                            </thead>
+                            <tbody>
+
+                                {
+
+
+                                    data.listarUsuarios.map((usuario, index) => (
+                                        <tr key={usuario._id}>
+                                            {/* <td>{usuario._id}</td> */}
+                                            <td>{usuario.nombre}</td>
+                                            <td>{usuario.rol}</td>
+                                            <td>{usuario.correo}</td>
+                                            {/* <td><select >
+                                                <option>Pendiente</option>
+                                                <option>Autorizado</option>
+                                                <option>No Autorizado</option>
+                                            </select></td> */}
+                                        </tr>
+
+                                    ))
+
+
+
+
+                                }
+
+                            </tbody>
+                        </table>
+
+                        <div class="acciones" align="center">
+                            <h5>
+                                <button class="Guardar">
+                                    Guardar
+                                </button>
+                            </h5>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+
+
+
+            }
+
+
+        </>
+
     )
+
 }
 
 export default Gestionusuarios
